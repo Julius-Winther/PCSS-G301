7f338e09866f7dfd8f092c1e27dd581106b703a7
mfj4000 <mjager20@student.aau.dk>
2021-10-25 14:43:48 +0200

    Commit client hehe

--- Back end/DummyClient/src/GameInstance.java
+++ Back end/DummyClient/src/GameInstance.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class GameInstance {
-
+ //hehe
     public void joinServer(String host, int port) throws IOException {
         Socket socket = new Socket(host, port);
     }
