d6616d07332cc07791beb231a9dcd553be0e467b
mfj4000 <mjager20@student.aau.dk>
2021-10-25 14:36:37 +0200

    Commit client hehe

--- Back end/DummyClient/src/Main.java
+++ Back end/DummyClient/src/Main.java
@@ -8,19 +8,28 @@ public class Main {
 
         Scanner scanner = new Scanner (System.in);
 
-        while(true) {
-            System.out.println("Write start to join the server");
-            String userInput = scanner.next();
-            System.out.println(userInput);
+        boolean hasJoinedHost = false;
 
-            if(userInput.equals("start")) {
-                System.out.println("Write IP-Address: ");
-                String hostInput = scanner.next();
-                System.out.println("Write Port: ");
-                String portInput = scanner.next();
-                gameInstance.joinServer(hostInput, Integer.parseInt(portInput));
+        while(true) {
+
+            if (hasJoinedHost){
+                System.out.println("I am in a game");
+                String userInput = scanner.next();
+            }
+            else {
+                System.out.println("Write start to join the server");
+                String userInput = scanner.next();
+                System.out.println(userInput);
+
+                if(userInput.equals("start")) {
+                    System.out.println("Write IP-Address: ");
+                    String hostInput = scanner.next();
+                    System.out.println("Write Port: ");
+                    String portInput = scanner.next();
+                    gameInstance.joinServer(hostInput, Integer.parseInt(portInput));
+                }
+                hasJoinedHost = true;
             }
         }
-
     }
 }
