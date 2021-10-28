cee9efa90de82adf8c88cc8ba07fd7db7aa7c806
Maya-Bordum <72729930+Maya-Bordum@users.noreply.github.com>
2021-10-28 15:09:24 +0200

    fxml changes

--- FrontEndMiniprojectDina/src/main/java/frontendminiproject/frontendminiprojectdina/Main.java
+++ FrontEndMiniprojectDina/src/main/java/frontendminiproject/frontendminiprojectdina/Main.java
@@ -16,9 +16,11 @@ import java.io.IOException;
 
 public class Main extends Application {
 
+    private boolean IsHost = false;
+
     //All buttons or other elements are defined here.
     @FXML
-    private Button joinButton, hostButton;
+    private Button joinButton,PBPlus1,PBPlus2,PBPlus3,PBMinus1,PBMinus2,PBMinus3;
 
 
     //This is the main start function.
@@ -29,6 +31,7 @@ public class Main extends Application {
 
         Scene homeScene = new Scene(homePage.load(), 1080, 600);
 
+        stage.setTitle("Jeoparty");
         stage.setScene(homeScene);
         stage.show();
     }
@@ -48,8 +51,19 @@ public class Main extends Application {
             stage.show();
         }
 
+        if(IsHost)
+        {
+            if(event.getSource() == PBPlus1 || event.getSource() == PBPlus2 || event.getSource() == PBPlus3)
+            {
+                
+            }
+        }
     }
 
+    private void setPointsForPlayer()
+    {
+
+    }
 
     public static void main(String[] args)
     {
