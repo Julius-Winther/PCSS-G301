5e0ef3f40d5dacac1d58c18ad486b6e007a70fb4
MikkelMed <mafl20@student.aau.dk>
2021-11-02 14:33:01 +0100

    expanding on capabilities of my server/client testing

--- Back end/LausForsimpling/LauClient/src/Jeoparty.java
+++ Back end/LausForsimpling/LauClient/src/Jeoparty.java
@@ -10,7 +10,7 @@ public class Jeoparty {
     int port;
     int clientID;
 
-    boolean hasJoinedServer, isHost, hasJoinedLobby;
+    boolean hasJoinedServer, isHost, hasJoinedLobby, gameHasStarted;
 
     DataInputStream input;
     DataOutputStream output;
@@ -23,9 +23,9 @@ public class Jeoparty {
         Scanner scanner = new Scanner(System.in);
 
         while(true) {
-            if(isHost) {
+            if(isHost && !hasJoinedLobby) {
                 System.out.println("You are the host");
-            }
+            } //prints out that you are the host
 
             if(hasJoinedServer && !hasJoinedLobby) {
                 System.out.println("Name:");
@@ -35,7 +35,7 @@ public class Jeoparty {
                     output.writeUTF(name);
                     hasJoinedLobby = true;
                 }
-            }
+            } //asks the user for a name and then creates a player and notifies the server
             else {
                 System.out.println("IP-Address:");
                 host = scanner.next();
@@ -44,14 +44,25 @@ public class Jeoparty {
                 port = scanner.nextInt();
 
                 joinServer(host, port);
-            }
+            } //the user is prompted to provide ip and port for them to join
 
             if(hasJoinedLobby) {
                 if(isHost) {
                     System.out.println("Write 'start' to start the game");
+                    if(scanner.next().equals("start")) {
+                        output.writeBoolean(true); //host is starting game
+                    }
                 }
-            }
+                else {
+                    System.out.println("Waiting for host to start game...");
+                    gameHasStarted = input.readBoolean();
+                }
+            } //if is host, prompts you accordingly while in the lobby
         }
+    }
+
+    public void game() {
+        System.out.println("Your mom");
     }
 
     public void joinServer(String host, int port) throws IOException {
